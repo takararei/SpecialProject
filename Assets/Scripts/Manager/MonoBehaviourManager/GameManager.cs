0b9e470d99c25212952abf6aa861cb6e855fa130
王宝玲\Administrator <327967531@qq.com>
2019-01-18 14:11:35 +0800

    提交说明: 1.添加场景状态基类 2.添加各种Manager

---
+++ Assets/Scripts/Manager/MonoBehaviourManager/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public PlayerManager playerManager;
+    public FactoryManager factoryManger;
+    public AudioSourceManager audioSourceManger;
+    public UIManager uiManager;
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+        playerManager = new PlayerManager();
+        factoryManger = new FactoryManager();
+        audioSourceManger = new AudioSourceManager();
+        uiManager = new UIManager();
+
+    }
+}
